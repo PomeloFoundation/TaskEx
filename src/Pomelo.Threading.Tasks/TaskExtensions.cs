ba680081d5b266e5fafdf3a3313d8089ef617311
雨宮丶優子 <911574351@qq.com>
2016-10-30 19:46:59 +0800

    Init

---
+++ src/Pomelo.Threading.Tasks/TaskExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System.Threading.Tasks
+{
+    public static class TaskEx
+    {
+        public static async Task<T> WaitAny<T>(Func<T, bool> Predicate, ICollection<Task<T>> Tasks)
+        {
+            while (Tasks.Count() > 0)
+            {
+                var finishedTask = await Task.WhenAny(Tasks);
+                Tasks.Remove(finishedTask);
+                if (Predicate(await finishedTask))
+                    return await finishedTask;
+            }
+            return default(T);
+        }
+
+        public static Task<T> WaitAny<T>(Func<T, bool> Predicate, params Task<T>[] Tasks) => WaitAny(Predicate, Tasks);
+
+        public static async Task<Task<T>> WhenAny<T>(Func<T, bool> Predicate, ICollection<Task<T>> Tasks)
+        {
+            while(Tasks.Count() > 0)
+            {
+                var finishedTask = await Task.WhenAny(Tasks);
+                Tasks.Remove(finishedTask);
+                if (Predicate(await finishedTask))
+                    return finishedTask;
+            }
+            return null;
+        }
+
+        public static Task<Task<T>> WhenAny<T>(Func<T, bool> Predicate, params Task<T>[] Tasks) => WhenAny(Predicate, Tasks);
+    }
+}
